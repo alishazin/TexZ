fdce9b3d25dcf1a505319f25ef4781082cbd7b05
alishazin <78612608+alishazin@users.noreply.github.com>
2024-01-17 19:33:03 +0530

    added delete and info button

--- client/src/components/MessageContainer.jsx
+++ client/src/components/MessageContainer.jsx
@@ -1,15 +1,25 @@
 import "../styles/components/messagecontainer.css"
+import { Icon } from '@iconify/react'
 
 function MessageContainer({ side, msg, name, date, time }) {
 
     return (
         <div className={`single-message-container ${side}`}>
-            <div className="msg-area">{msg}</div>
-            <div className="details-area">
-                <div className="name">{name}</div>
-                <div className="date">{date}</div>
-                <div className="time">{time}</div>
+            {side === "right" && <div className="transparent-area">
+                <div className="box"><Icon icon="material-symbols:delete-outline" className="icon" /></div>
+                <div className="box"><Icon icon="material-symbols:info-outline" className="icon" /></div>
+            </div>}
+            <div className="opaque-area">
+                <div className="msg-area">{msg}</div>
+                <div className="details-area">
+                    <div className="name">{name}</div>
+                    <div className="date">{date}</div>
+                    <div className="time">{time}</div>
+                </div>
             </div>
+            {side === "left" && <div className="transparent-area">
+                <Icon icon="material-symbols:info-outline" className="icon" />
+            </div>}
         </div>
     )
 }
